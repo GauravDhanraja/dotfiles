1128e0821ca1015f166fb7200ae9b79c4edf6564
GauravDhanraja <gauravdhanraja88@proton.me>
2025-01-20 03:50:17 +0530

    Set lualine theme to 'auto' and remove left seperator from mode status

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,284 +1,257 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-		config = function()
-			vim.cmd.colorscheme("tokyonight-night")
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
-				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown" },
-				sync_install = false,
-				auto_install = true,
-				ignore_install = {},
-				modules = {},
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
-			})
-		end,
-	},
-	{
-		{
-			"nvim-telescope/telescope.nvim",
-			tag = "0.1.6",
-			dependencies = { "nvim-lua/plenary.nvim" },
-			config = function()
-				local builtin = require("telescope.builtin")
-				vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-				vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-				vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-				vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-			end,
-		},
-		{
-			"nvim-telescope/telescope-ui-select.nvim",
-			config = function()
-				require("telescope").setup({
-					defaults = {
-						file_ignore_patterns = {
-							"node_modules",
-						},
-					},
-					extensions = {
-						["ui-select"] = {
-							require("telescope.themes").get_dropdown({}),
-						},
-					},
-				})
-				require("telescope").load_extension("ui-select")
-			end,
-		},
-	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-		config = function()
-			vim.keymap.set("n", "<leader>n", ":Neotree toggle filesystem<CR>", {})
-		end,
-	},
-	{
-		"stevearc/oil.nvim",
-		opts = {},
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("oil").setup()
-			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-		end,
-	},
-	{
-		"jiaoshijie/undotree",
-		dependencies = "nvim-lua/plenary.nvim",
-		keys = {
-			{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
-		},
-		config = function()
-			local undotree = require("undotree")
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      vim.cmd.colorscheme("tokyonight-night")
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown" },
+        sync_install = false,
+        auto_install = true,
+        ignore_install = {},
+        modules = {},
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+      })
+    end,
+  },
+  {
+    {
+      "nvim-telescope/telescope.nvim",
+      tag = "0.1.6",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      config = function()
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+        vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+        vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+        vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+      end,
+    },
+    {
+      "nvim-telescope/telescope-ui-select.nvim",
+      config = function()
+        require("telescope").setup({
+          defaults = {
+            file_ignore_patterns = {
+              "node_modules",
+            },
+          },
+          extensions = {
+            ["ui-select"] = {
+              require("telescope.themes").get_dropdown({}),
+            },
+          },
+        })
+        require("telescope").load_extension("ui-select")
+      end,
+    },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      vim.keymap.set("n", "<leader>n", ":Neotree toggle filesystem<CR>", {})
+    end,
+  },
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("oil").setup()
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    end,
+  },
+  {
+    "jiaoshijie/undotree",
+    dependencies = "nvim-lua/plenary.nvim",
+    keys = {
+      { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+    },
+    config = function()
+      local undotree = require("undotree")
 
-			undotree.setup({
-				float_diff = true, -- using float window previews diff, set this `true` will disable layout option
-				layout = "left_bottom", -- "left_bottom", "left_left_bottom"
-				position = "left", -- "right", "bottom"
-				ignore_filetype = {
-					"undotree",
-					"undotreeDiff",
-					"qf",
-					"TelescopePrompt",
-					"spectre_panel",
-					"tsplayground",
-				},
-				window = {
-					winblend = 30,
-				},
-				keymaps = {
-					["j"] = "move_next",
-					["k"] = "move_prev",
-					["gj"] = "move2parent",
-					["J"] = "move_change_next",
-					["K"] = "move_change_prev",
-					["<cr>"] = "action_enter",
-					["p"] = "enter_diffbuf",
-					["q"] = "quit",
-				},
-			})
-		end,
-	},
-	{
-		"theprimeagen/harpoon",
-		branch = "harpoon2",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("harpoon"):setup()
-		end,
-		menu = {
-			width = vim.api.nvim_win_get_width(0) - 4,
-		},
-		keys = {
-			{
-				"<leader>a",
-				function()
-					require("harpoon"):list():add()
-				end,
-				desc = "harpoon file",
-			},
-			{
-				"<C-e>",
-				function()
-					local harpoon = require("harpoon")
-					harpoon.ui:toggle_quick_menu(harpoon:list())
-				end,
-				desc = "harpoon quick menu",
-			},
-			{
-				"<leader>1",
-				function()
-					require("harpoon"):list():select(1)
-				end,
-				desc = "harpoon to file 1",
-			},
-			{
-				"<leader>2",
-				function()
-					require("harpoon"):list():select(2)
-				end,
-				desc = "harpoon to file 2",
-			},
-			{
-				"<leader>3",
-				function()
-					require("harpoon"):list():select(3)
-				end,
-				desc = "harpoon to file 3",
-			},
-			{
-				"<leader>4",
-				function()
-					require("harpoon"):list():select(4)
-				end,
-				desc = "harpoon to file 4",
-			},
-			{
-				"<leader>5",
-				function()
-					require("harpoon"):list():select(5)
-				end,
-				desc = "harpoon to file 5",
-			},
-		},
-	},
-	{
-		{
-			"lewis6991/gitsigns.nvim",
-			config = function()
-				require("gitsigns").setup()
-			end,
-		},
-		{
-			"tpope/vim-fugitive",
-		},
-		{
-			"kdheepak/lazygit.nvim",
-			cmd = {
-				"LazyGit",
-				"LazyGitConfig",
-				"LazyGitCurrentFile",
-				"LazyGitFilter",
-				"LazyGitFilterCurrentFile",
-			},
-			dependencies = {
-				"nvim-lua/plenary.nvim",
-			},
-			keys = {
-				{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-			},
-		},
-	},
-	{
-		{
-			"nvim-lualine/lualine.nvim",
-			dependencies = { "nvim-tree/nvim-web-devicons" },
-			config = function()
-				local colors = {
-					blue = "#80a0ff",
-					cyan = "#79dac8",
-					black = "#080808",
-					white = "#fffff",
-					red = "#ff5189",
-					violet = "#d183e8",
-					grey = "#585b70",
-				}
-
-				local bubbles_theme = {
-					normal = {
-						a = { fg = colors.black, bg = colors.violet },
-						b = { fg = colors.white, bg = colors.grey },
-						c = { fg = colors.white },
-					},
-
-					insert = { a = { fg = colors.black, bg = colors.blue } },
-					visual = { a = { fg = colors.black, bg = colors.cyan } },
-					replace = { a = { fg = colors.black, bg = colors.red } },
-
-					inactive = {
-						a = { fg = colors.white, bg = colors.black },
-						b = { fg = colors.white, bg = colors.black },
-						c = { fg = colors.white },
-					},
-				}
-				require("lualine").setup({
-					options = {
-						icons_enabled = true,
-						theme = bubbles_theme,
-						component_separators = "",
-						section_separators = { left = "", right = "" },
-						disabled_filetypes = {
-							statusline = {},
-							winbar = {},
-						},
-						ignore_focus = {},
-						always_divide_middle = true,
-						globalstatus = false,
-						refresh = {
-							statusline = 200,
-							tabline = 200,
-							winbar = 200,
-						},
-					},
-					sections = {
-						lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
-						lualine_b = { "branch", "diff", "diagnostics" },
-						lualine_c = { "filename" },
-						lualine_x = { "encoding", "fileformat", "filetype" },
-						lualine_y = { "progress" },
-						lualine_z = { "location" },
-					},
-					inactive_sections = {
-						lualine_a = {},
-						lualine_b = {},
-						lualine_c = { "filename" },
-						lualine_x = { "location" },
-						lualine_y = {},
-						lualine_z = {},
-					},
-					tabline = {},
-					winbar = {},
-					inactive_winbar = {},
-					extensions = {},
-				})
-			end,
-		},
-	},
+      undotree.setup({
+        float_diff = true,  -- using float window previews diff, set this `true` will disable layout option
+        layout = "left_bottom", -- "left_bottom", "left_left_bottom"
+        position = "left",  -- "right", "bottom"
+        ignore_filetype = {
+          "undotree",
+          "undotreeDiff",
+          "qf",
+          "TelescopePrompt",
+          "spectre_panel",
+          "tsplayground",
+        },
+        window = {
+          winblend = 30,
+        },
+        keymaps = {
+          ["j"] = "move_next",
+          ["k"] = "move_prev",
+          ["gj"] = "move2parent",
+          ["J"] = "move_change_next",
+          ["K"] = "move_change_prev",
+          ["<cr>"] = "action_enter",
+          ["p"] = "enter_diffbuf",
+          ["q"] = "quit",
+        },
+      })
+    end,
+  },
+  {
+    "theprimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("harpoon"):setup()
+    end,
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
+    keys = {
+      {
+        "<leader>a",
+        function()
+          require("harpoon"):list():add()
+        end,
+        desc = "harpoon file",
+      },
+      {
+        "<C-e>",
+        function()
+          local harpoon = require("harpoon")
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "harpoon quick menu",
+      },
+      {
+        "<leader>1",
+        function()
+          require("harpoon"):list():select(1)
+        end,
+        desc = "harpoon to file 1",
+      },
+      {
+        "<leader>2",
+        function()
+          require("harpoon"):list():select(2)
+        end,
+        desc = "harpoon to file 2",
+      },
+      {
+        "<leader>3",
+        function()
+          require("harpoon"):list():select(3)
+        end,
+        desc = "harpoon to file 3",
+      },
+      {
+        "<leader>4",
+        function()
+          require("harpoon"):list():select(4)
+        end,
+        desc = "harpoon to file 4",
+      },
+      {
+        "<leader>5",
+        function()
+          require("harpoon"):list():select(5)
+        end,
+        desc = "harpoon to file 5",
+      },
+    },
+  },
+  {
+    {
+      "lewis6991/gitsigns.nvim",
+      config = function()
+        require("gitsigns").setup()
+      end,
+    },
+    {
+      "tpope/vim-fugitive",
+    },
+    {
+      "kdheepak/lazygit.nvim",
+      cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+      },
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+      },
+      keys = {
+        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+      },
+    },
+  },
+  {
+    {
+      "nvim-lualine/lualine.nvim",
+      dependencies = { "nvim-tree/nvim-web-devicons" },
+      config = function()
+        require("lualine").setup({
+          options = {
+            icons_enabled = true,
+            theme = 'auto',
+            component_separators = "",
+            section_separators = { left = "", right = "" },
+            disabled_filetypes = {
+              statusline = {},
+              winbar = {},
+            },
+            ignore_focus = {},
+            always_divide_middle = true,
+            globalstatus = false,
+            refresh = {
+              statusline = 200,
+              tabline = 200,
+              winbar = 200,
+            },
+          },
+          sections = {
+            lualine_a = { { "mode", right_padding = 2 } },
+            lualine_b = { "branch", "diff", "diagnostics" },
+            lualine_c = { "filename" },
+            lualine_x = { "encoding", "fileformat", "filetype" },
+            lualine_y = { "progress" },
+            lualine_z = { "location" },
+          },
+          inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = { "filename" },
+            lualine_x = { "location" },
+            lualine_y = {},
+            lualine_z = {},
+          },
+          tabline = {},
+          winbar = {},
+          inactive_winbar = {},
+          extensions = {},
+        })
+      end,
+    },
+  },
 }
